a3a063b78d4bde5d003a6ff0df2bb143bb98636e
Nafshiyat Nihal <nihalnaf@msu.edu>
2022-11-14 08:07:42 -0500

    Finished lab

---
+++ soundboard.js
@@ -0,0 +1,29 @@
+var buttonAhem = document.getElementById('ahem-button')
+buttonAhem.addEventListener('click', function () {
+    var audioAhem = document.getElementById('ahem-audio')
+    audioAhem.play()
+  })
+  buttonAhem.addEventListener('mouseenter', function () {
+    var audioAhem = document.getElementById('ahem-audio')
+    audioAhem.play()
+  })
+
+var buttonApplause = document.getElementById('applause-button')
+buttonApplause.addEventListener('click', function () {
+    var audioApplause = document.getElementById('applause-audio')
+    audioApplause.play()
+  })
+  buttonApplause.addEventListener('mouseenter', function () {
+    var audioApplause = document.getElementById('applause-audio')
+    audioApplause.play()
+  })
+
+var buttonArrow = document.getElementById('arrow-button')
+buttonArrow.addEventListener('click', function () {
+    var audioArrow = document.getElementById('arrow-audio')
+    audioArrow.play()
+  })
+  buttonArrow.addEventListener('mouseenter', function () {
+    var audioArrow = document.getElementById('arrow-audio')
+    audioArrow.play()
+  })
